fe0862e4999f69d189e82bc9e4e04f86d262e456
Thiago Montezuma <33211006+thiagomontezuma@users.noreply.github.com>
2023-01-14 13:36:06 -0500

    Major changes on the whole website - v2.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind()],
-  site: "https://psoems.com/"
+  integrations: [sitemap(), tailwind(), mdx()],
+  site: 'https://psoems.com/'
 });
